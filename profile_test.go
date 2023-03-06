4868ea0b63b464d57dcc50bd6bf6555ab1e52948
Gkirito <lownkid@outlook.com>
2023-03-06 11:34:09 +0800

    🧪 test(profile_test): add test

---
+++ profile_test.go
@@ -0,0 +1,93 @@
+package zkgroup
+
+import (
+	"encoding/base64"
+	"reflect"
+	"testing"
+)
+
+func TestCreateProfileKeyCredentialRequestContext(t *testing.T) {
+	zkGroupServerPublicParams, _ := base64.StdEncoding.DecodeString("AMh1gu/ongPtTUjIejLX8fWKvJo5HkW6ajb5X5IGq0dABBjMz4KPsJYZ5BJEAavUMC7d8qHyAGUiRs4uIlwubQ4qVhlpEZtd8jIDDHgS0Bqi0RXr9B9fcw8wrNoEcdcnX7hnOuZV/8nZQ1WQdmNiP7LR7EvTeFk/iEj7/UV7bux1pjvSNq4E964apj+2Pux2Xo9kNctJ0oWehW/3vujoiy8=")
+	type args struct {
+		serverPublicParams []byte
+		uuid               []byte
+		profileKey         []byte
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    ProfileKeyCredentialRequestContext
+		wantErr bool
+	}{
+		{
+			name: "test1",
+			args: args{
+				serverPublicParams: zkGroupServerPublicParams,
+				uuid: []byte{
+					165,
+					99,
+					46,
+					73,
+					204,
+					142,
+					71,
+					25,
+					164,
+					54,
+					154,
+					16,
+					54,
+					166,
+					72,
+					73,
+				},
+				profileKey: []byte{
+					178,
+					41,
+					47,
+					237,
+					227,
+					49,
+					131,
+					193,
+					136,
+					139,
+					128,
+					56,
+					228,
+					178,
+					107,
+					165,
+					69,
+					52,
+					145,
+					68,
+					97,
+					162,
+					104,
+					122,
+					3,
+					64,
+					54,
+					42,
+					153,
+					248,
+					76,
+					204,
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := CreateProfileKeyCredentialRequestContext(tt.args.serverPublicParams, tt.args.uuid, tt.args.profileKey)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("CreateProfileKeyCredentialRequestContext() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("CreateProfileKeyCredentialRequestContext() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
